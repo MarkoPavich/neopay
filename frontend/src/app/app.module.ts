3f3e29b7692a5bf60f455fa90e8d430d8c1b3ce5
= <marko.pavic667@gmail.com>
2021-07-27 19:51:28 +0200

    refactor: move nav to layout subdir;

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
