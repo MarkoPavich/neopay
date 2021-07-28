78a2b04dd3dbfed4b3fb25fac33f9fb565702cfa
= <marko.pavic667@gmail.com>
2021-07-28 19:41:21 +0200

    define main content containers;

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { InvoicesComponent } from './components/pages/invoices/invoices.component';
+import { PageFrameComponent } from './components/layout/page-frame/page-frame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    InvoicesComponent,
+    PageFrameComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/layout/page-frame/page-frame.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageFrameComponent } from './page-frame.component';
+
+describe('PageFrameComponent', () => {
+  let component: PageFrameComponent;
+  let fixture: ComponentFixture<PageFrameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageFrameComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageFrameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
