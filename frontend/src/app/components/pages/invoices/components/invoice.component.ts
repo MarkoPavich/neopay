53e4d733ddaf566fc3f673d28b601082ca11044d
= <marko.pavic667@gmail.com>
2021-07-29 20:05:58 +0200

    partial content and styling to invoiceComponent;

--- frontend/src/app/components/pages/invoices/components/invoice.component.ts
+++ frontend/src/app/components/pages/invoices/components/invoice.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'invoice',
   template: `
+    
+    <div class="content-container">
+      <div class="container-left">
+        <span class="hash-prefix">#</span>
+        <span>RTX3080</span>
+      </div>
+      <div class="container-middle">
+        <span>Due  19 Aug 2021</span>
+        <span>Jensen Huang</span>
+      </div>
+      <div class="container-right">
+        <div class="price-box">
+          <span>€</span>
+          <span>1,800.90</span>
+        </div>
+        <div class="status-box">
 
-
+        </div>
+      </div>
+    </div>
     `,
   styleUrls: ['./invoice.component.scss']
 })
