896a7bc80b32671da5b011b59a500b556a8a4259
= <marko.pavic667@gmail.com>
2021-07-29 18:41:49 +0200

    add hero button basic styling;

--- frontend/src/app/components/shared/buttons/button.hero.component.ts
+++ frontend/src/app/components/shared/buttons/button.hero.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'button-hero',
   template: `
+
   <div class="button-hero">
-    <div class="add-circle">+</div>
+    <div class="add-circle">
+      <img src="/assets/icon-plus.svg" alt="add.img">
+    </div>
     <button>New Invoice</button>
   </div>
+
   `,
   styleUrls: ['./button.hero.component.scss']
 })
