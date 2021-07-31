1c6f5a36d09e314df3f1e157faf7e4bb1bd4c4a1
= <marko.pavic667@gmail.com>
2021-07-31 13:10:59 +0200

    add invoiceFormComponent with some basic styling;

---
+++ frontend/src/app/components/pages/invoices/components/invoice-form-component/invoice.form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'invoice-form',
+  templateUrl: './invoice.form.component.html',
+  styleUrls: ['./invoice.form.component.scss']
+})
+
+export class InvoiceFormComponent {}
--- frontend/src/app/components/pages/invoices/invoices.module.ts
+++ frontend/src/app/components/pages/invoices/invoices.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
+import { SharedComponentsModule } from '../../shared/shared.components.module.ts';
 
 import { InvoicesListComponent } from './invoices.list.component';
 import { InvoiceComponent } from './components/invoice.component';
 import { StatusInfoButtonComponent } from './components/status.info.button.component';
 import { InvoicesHeaderComponent } from './components/invoices.header.component'
-import { SharedComponentsModule } from '../../shared/shared.components.module.ts';
+import { InvoiceFormComponent } from './components/invoice-form-component/invoice.form.component';
 
 @NgModule({
   declarations: [
     InvoicesListComponent,
     InvoicesHeaderComponent,
     InvoiceComponent,
-    StatusInfoButtonComponent
+    StatusInfoButtonComponent,
+    InvoiceFormComponent
   ],
   exports: [
     InvoicesListComponent
