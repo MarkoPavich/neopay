5d2b1bd5ee6d0b1ae07f099cab221be532a70df4
= <marko.pavic667@gmail.com>
2021-10-17 11:07:47 +0200

    add temp logout feat;

--- frontend/src/app/components/layout/navbar/navbar.component.ts
+++ frontend/src/app/components/layout/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/auth/session.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  constructor(private sessionService: SessionService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onLogout() {
+    this.sessionService.clearSession();
+    this.router.navigate(['login']);
   }
-
 }
