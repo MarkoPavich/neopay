9d7185f2a07ec49849fdb11f5e543257818e3381
= <marko.pavic667@gmail.com>
2021-10-24 19:53:44 +0200

    update invoiceListView no-content handling while loading;

--- frontend/src/app/components/pages/invoices-list/invoices.list.component.ts
+++ frontend/src/app/components/pages/invoices-list/invoices.list.component.ts
@@ -1,39 +1,50 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { Invoice } from 'src/app/models/models';
+import { SessionService } from 'src/app/services/auth/session.service';
 import { InvoiceService } from 'src/app/services/http/invoice.service';
 import { InvoiceFormComponent } from '../../shared/invoice-form-component/invoice.form.component';
 
 @Component({
   selector: 'invoices-list-component',
   templateUrl: './invoices.list.component.html',
-  styleUrls: ['./invoices.list.component.scss']
+  styleUrls: ['./invoices.list.component.scss'],
 })
 export class InvoicesListComponent implements OnInit {
   @ViewChild(InvoiceFormComponent) _invoiceForm!: InvoiceFormComponent;
 
   private _invoices: Invoice[] = [];
+  private _isLoading: boolean = false;
 
-  constructor(private service: InvoiceService) { }
+  constructor(private service: InvoiceService) {}
 
   ngOnInit() {
     this.getInvoices();
   }
 
-  get invoices(): Invoice[]{
+  get invoices(): Invoice[] {
     return this._invoices;
   }
 
-  getInvoices(){
-    this.service.get().subscribe((invoices: Invoice[]) => {
-      this._invoices = invoices;
-    })
+  get isLoading(): boolean {
+    return this._isLoading;
   }
 
-  handleOpenNew(){
+  getInvoices() {
+    this._isLoading = true;
+    this.service.get().subscribe(
+      (invoices: Invoice[]) => {
+        this._isLoading = false;
+        this._invoices = invoices;
+      },
+      (error) => (this._isLoading = false)
+    );
+  }
+
+  handleOpenNew() {
     this._invoiceForm.openNewForm();
   }
 
-  onSuccess(invoice: Invoice){
+  onSuccess(invoice: Invoice) {
     this._invoices.push(invoice);
   }
 }
