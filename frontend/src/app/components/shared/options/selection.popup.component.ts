5749634b9fc953e9f5ba2619fbc7d911145d471c
Marko <marko.pavic667@gmail.com>
2021-11-27 19:56:33 +0100

    more progress on darkMode;

--- frontend/src/app/components/shared/options/selection.popup.component.ts
+++ frontend/src/app/components/shared/options/selection.popup.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'selection-popup-menu',
   template: `
-
     <div class="content-container">
       <span class="span-full-width">Filter by status</span>
       <span class="span-short">Filter</span>
-      <img src="/assets/icon-arrow-down.svg" alt="menu_arrow.img">
+      <img src="/assets/icon-arrow-down.svg" alt="menu_arrow.img" />
     </div>
-    `,
-  styleUrls: ['./selection.popup.component.scss']
+  `,
+  styleUrls: ['./selection.popup.component.scss'],
 })
 export class SelectionPopupComponent {}
