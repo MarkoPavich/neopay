62d45d8762c944e90f78157969ac14399c94393b
Marko <marko.pavic667@gmail.com>
2021-11-27 20:16:24 +0100

    store theme preference to localstorage;

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private _darkThemeEnabled: boolean = false;
 
   get darkThemeEnabled(): boolean {
     return this._darkThemeEnabled;
   }
 
+  ngOnInit() {
+    const darkModePref = localStorage.getItem('dark-mode');
+    this._darkThemeEnabled = darkModePref ? JSON.parse(darkModePref) : false;
+  }
+
   handleThemeChange() {
     this._darkThemeEnabled = !this._darkThemeEnabled;
+    localStorage.setItem('dark-mode', JSON.stringify(this._darkThemeEnabled));
   }
 }
