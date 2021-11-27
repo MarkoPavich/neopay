c5b16e7914790e47910719e04cc065ae58496913
Marko <marko.pavic667@gmail.com>
2021-11-27 20:03:59 +0100

    plug theme select icon;

--- frontend/src/app/components/layout/navbar/navbar.component.ts
+++ frontend/src/app/components/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionService } from 'src/app/services/auth/session.service';
 
@@ -8,9 +8,14 @@ import { SessionService } from 'src/app/services/auth/session.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  @Input('isDarkTheme') _isDarkTheme: boolean = false;
   @Output('themeSelect') _themeEmitter = new EventEmitter<void>();
 
   constructor(private sessionService: SessionService, private router: Router) {}
+
+  get isDarkTheme(): boolean{
+    return this._isDarkTheme;
+  }
 
   onThemeChange() {
     this._themeEmitter.emit();
