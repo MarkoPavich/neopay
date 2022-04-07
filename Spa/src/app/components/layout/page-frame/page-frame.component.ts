55a773592afad22cbef897edc500d9215ba26c17
Marko Pavić <marko.pavic667@gmail.com>
2022-04-07 22:05:41 +0200

    minor cleanup;

--- Spa/src/app/components/layout/page-frame/page-frame.component.ts
+++ Spa/src/app/components/layout/page-frame/page-frame.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-frame',
   templateUrl: './page-frame.component.html',
   styleUrls: ['./page-frame.component.scss']
 })
-export class PageFrameComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class PageFrameComponent {}
